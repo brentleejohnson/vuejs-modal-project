f7c53f120c7281b1a05c186922b7690cb99eb5e2
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-14 13:48:58 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
-  <div v-if="showModal">
+
+  <teleport to="#modals" v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <template v-slot:links>
-        <a href="#">Sign Up Now</a>
-        <a href="#">More Info</a>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
       </template>
-      <h1>Ninja Giveaway</h1>
+      <h1>Ninja Givaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
-  </div>
-  <button @click.alt="toggleModal">Open Modal (Alt)</button>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the Newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+
+  <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
@@ -22,22 +32,24 @@ export default {
   data() {
     return {
       title: "My First Vue App!",
-      header: "Sign up for the Giveaway",
-      text: "Grab your ninja swag for half price!",
       showModal: false,
+      showModalTwo: false,
     };
   },
-
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
     },
   },
 };
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -45,10 +57,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+button {
+  background: #bbb;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  margin: 10px;
+  font-size: 16px;
+  color: #333;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <slot></slot>
+      <slot>default content (if no slot passed in)</slot>
       <div class="actions">
         <slot name="links"></slot>
       </div>
@@ -28,7 +28,6 @@ export default {
   background: white;
   border-radius: 10px;
 }
-
 .backdrop {
   top: 0;
   position: fixed;
@@ -36,24 +35,20 @@ export default {
   width: 100%;
   height: 100%;
 }
-
 .modal h1 {
   color: #03cfb4;
   border: none;
   padding: 0;
 }
-
 .modal p {
   font-style: normal;
 }
-
-.modal.actions {
+.modal .actions {
   text-align: center;
   margin: 30px 0 10px 0;
   color: #333;
 }
-
-.modal.actions a {
+.modal .actions a {
   color: #333;
   padding: 8px;
   border: 1px solid #eee;
@@ -61,21 +56,18 @@ export default {
   text-decoration: none;
   margin: 10px;
 }
-
+/* sale styles */
 .modal.sale {
   background: crimson;
   color: white;
 }
-
 .modal.sale h1 {
   color: white;
 }
-
-.modal.sale.actions {
+.modal.sale .actions {
   color: white;
 }
-
-.modal.sale.actions a {
+.modal.sale .actions a {
   color: white;
 }
 </style>
